c65bfef86d95b6615c18eedccc7023d1ce9a1b7e
Jake Tekalec <jaketekalec@gmail.com>
2022-04-27 01:12:33 -0400

    Added functional distance-based guessing

--- colorle.js
+++ colorle.js
@@ -10,13 +10,51 @@ function toRGBString(c) {
 	return 'rgb('+c[0]+','+c[1]+','+c[2]+')'
 }
 
+function toHexString(c) {
+	let r = padHex(c[0].toString(16))
+	let g = padHex(c[1].toString(16))
+	let b = padHex(c[2].toString(16))
+
+	return '#'+r+g+b;
+}
+
+function padHex(n) {
+	return n.length == 2 ? n : '0'+n
+}
+
+function hexToArr(c) {
+	if(c[0]=='#') {
+		c = c.substring(1)
+	}
+
+	let r = parseInt(c.substring(0,2),16);
+	let g = parseInt(c.substring(2,4),16);
+	let b = parseInt(c.substring(4,6),16);
+
+	return [r,g,b]
+}
+
 function colorDist(c1,c2) {
+	console.log(c1,c2)
 	return Math.sqrt(Math.pow(c1[0]-c2[0],2)+Math.pow(c1[1]-c2[1],2)+Math.pow(c1[2]-c2[2],2))
+}
+
+function getDist(e) {
+	if(event.key != 'Enter') return;
+	let g = hexToArr(document.getElementById("guess").value);
+	let a = hexToArr(document.getElementById("answer").value);
+
+	document.getElementById("dist").value = colorDist(g,a);
 }
 
 window.onload = function main() {
 	let c = randColor();
-	cStr = toRGBString(c)
-	document.getElementById("answer").value = cStr;
+	let cStr = toHexString(c);
+	let ansElem = document.getElementById("answer");
+	let guessElem = document.getElementById("guess")
+	ansElem.value = cStr;
+	//document.getElementById("guess").value = toHexString(hexToArr(cStr));
+	document.getElementById("dist").value = colorDist(c,c);
 	document.body.style.backgroundColor = cStr;
 }
+
