42922ab3402f532536acd93d7e33cb984a314103
Jake Tekalec <jaketekalec@gmail.com>
2022-04-27 00:23:50 -0400

    Added colorDist(c1,c2) and toRGBString(c)

--- colorle.js
+++ colorle.js
@@ -3,11 +3,20 @@ function randColor() {
 	let g = Math.floor(Math.random()*256);
 	let b = Math.floor(Math.random()*256);
 
-	return 'rgb('+r+','+g+','+b+')';
+	return [r,g,b];
+}
+
+function toRGBString(c) {
+	return 'rgb('+c[0]+','+c[1]+','+c[2]+')'
+}
+
+function colorDist(c1,c2) {
+	return Math.sqrt(Math.pow(c1[0]-c2[0],2)+Math.pow(c1[1]-c2[1],2)+Math.pow(c1[2]-c2[2],2))
 }
 
 window.onload = function main() {
 	let c = randColor();
-	document.getElementById("guess").value = c;
-	document.body.style.backgroundColor = c
+	cStr = toRGBString(c)
+	document.getElementById("answer").value = cStr;
+	document.body.style.backgroundColor = cStr;
 }
